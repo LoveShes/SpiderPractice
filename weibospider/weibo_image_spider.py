c213540403609d0cab8147208410c0e90225950b
LoveShes <hellopython3@foxmail.com>
2018-03-18 17:43:51 +0800

    更改业务逻辑，直接使用JSON数据获取图片URL

--- weibospider/weibo_image_spider.py
+++ weibospider/weibo_image_spider.py
@@ -1,41 +1,69 @@
 import requests
-import re
-import time
 import json
+import time
 import os
-from bs4 import BeautifulSoup
 
-def image_spider(weiboID):
-    url = 'https://m.weibo.cn/status/' + weiboID
-    html = requests.get(url)
-    html.encoding = 'utf-8'
-    soup = BeautifulSoup(html.text, 'html.parser')
-    imageID = soup.body.script.string.split('"pic_ids": [')[1].split('],')[0]
-    image_urlist = imageID.split(',')
-    user_path = 'weibo_spider/image/'
-    if not os.path.exists(user_path):
-        os.mkdir(user_path)
-    for image_url in image_urlist:
-        image_url = image_url.strip() 
-        image_url = image_url.replace('\n', '')
-        image_url = image_url.replace('"', '')
-        if (image_url):
-            url = 'https://wx4.sinaimg.cn/large/' + image_url + '.jpg'
-            image = requests.get(url, stream=True)  
-            with open(user_path + '{0}.jpg'.format(image_url), 'wb+') as f:
+def image_spider(image_url, date, index, user_name, image_count):
+    file_type = image_url.split('.')[-1]
+    file_path = 'weibo_spider/image/'  
+    image_path = 'weibo_spider/image/{0}/'.format(user_name)
+    image_name = image_path + date + '_{0:02}.{1}'.format(index,file_type)
+    if not os.path.exists(file_path):
+        os.mkdir(file_path)
+    if not os.path.exists(image_path):
+        os.mkdir(image_path)
+        print("目录创建成功！")
+        
+    if (image_url):
+        image = requests.get(image_url, stream=True) 
+        if not os.path.exists(image_name): 
+            with open(image_name, 'wb+') as f:
                 f.write(image.content)
+                print('\r成功下载第{0:08}张图片'.format(image_count), end='')
+        else:
+            print('\r第{0:08}张图片已经存在'.format(image_count), end='')
 
-
-def main_spider(ID, pages):
+def html_spider(ID, pages=500):
+    image_count = 0
     for n in range(1,pages+1):
         url = 'https://m.weibo.cn/api/container/getIndex?page={0}&type=uid&value={1}&containerid=107603{1}'.format(n,ID)
         html = requests.get(url)
         html_json = json.loads(html.text)
         cards = html_json['data']['cards']
+        if cards == []:
+            print('\n\n该用户全部图片下载完毕！')
+            break
         for card in cards:
-            if ('mblog' in card.keys()):
-                weiboID = card['mblog']['id']
-                image_spider(weiboID)
+            if ('mblog' in card):
+                date = card['mblog']['created_at']
+                user_name = card['mblog']['user']['screen_name']
+                if len(date) < 10:
+                    date = '2018-' + date
+                if ('pics' in card['mblog']):
+                    images = card['mblog']['pics']
+                    for index, image in enumerate(images):
+                        image_url = image['large']['url']
+                        image_count += 1
+                        image_spider(image_url, date, index+1, user_name, image_count)
+    time.sleep(2)   # 每爬取1页就暂停2秒 
+
+def main():
+    print("请输入想要爬取用户的主页地址，每个用户以换行符结束，输入q表示输入结束：")
+    users_list = []  
+    while True:  
+        users_url = input()      
+        if users_url == 'q' :
+            print('输入完毕！')
+            break
+        else:
+            users_list.append(users_url)    
+    for n, user_url in enumerate(users_list):
+        user_ID = user_url[-10:]
+        print('\n正在爬取第{0}个用户的微博图片...\n'.format(n+1))
+        html_spider(user_ID) 
+    print('\n全部用户爬取完毕！程序已退出运行')
 
 
-main_spider(2255058517,1)
+if __name__ == '__main__':
+    main()
+
